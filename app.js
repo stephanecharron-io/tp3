7b82ed8e67a2538bc2e427a2e114a7e8967fa129
stephanecharron <stephanecharron.web@gmail.com>
2018-11-19 07:01:16 -0500

    12

--- app.js
+++ app.js
@@ -2,6 +2,34 @@ const v3ApiKey = "71dddde08106498e1c93152088391560";
 const v4ApiKey = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MWRkZGRlMDgxMDY0OThlMWM5MzE1MjA4ODM5MTU2MCIsInN1YiI6IjViZWNmOTAzYzNhMzY4MTJmMzAwY2YyMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0FTwqDrzXxRhHcjQnquX6gKOCt1RumxgtGMjjRSOoZA";
 const apiurl = "https://api.themoviedb.org/3/movie/550?api_key=71dddde08106498e1c93152088391560";
 //https://codepen.io/kunalkamble/pen/XXbWwN
+//https://developers.google.com/web/fundamentals/web-components/
+//https://uigradients.com/#MoonlitAsteroid
+
+let DomData = {
+
+};
+
+class App {
+    constructor(){
+        this.modules = {};
+    }
+
+    addModule (name, module){
+        this.modules[name] = module;
+        document.getElementById(module.target).appendChild(module.composant);
+    }
+
+    getModule (name) {
+        return this.modules[name];
+    }
+}
+
+class Module {
+    constructor(target, composant){
+        this.target = target;
+        this.composant = composant;
+    }
+};
 
 class MovieElement extends HTMLElement {
 
@@ -29,20 +57,23 @@ class MovieElement extends HTMLElement {
         tmpElem.innerHTML =
             `<style type="text/css">
                 .title {
-                    color: red;
+                    color: white;
                 }   
 
                 :host {
                     display: block;
-                    border: 1px solid red;
+                    border: 1px solid white;
                     padding: 10px;
                     margin-bottom: 2px;
+                    border-radius: 3px;
                 }
             </style>`;
 
         return tmpElem.firstChild;
     }
 }
+customElements.define('movie-element', MovieElement);
+
 
 class MovielistElement extends HTMLElement {
 
@@ -60,29 +91,6 @@ class MovielistElement extends HTMLElement {
         }
     }
 }
-
-class App {
-    constructor(){
-        this.modules = {};
-    }
-
-    addModule (name, module){
-        this.modules[name] = module;
-        document.getElementById(module.target).appendChild(module.composant);
-    }
-
-    getModule (name) {
-        return this.modules[name];
-    }
-}
-
-class Module {
-    constructor(target, composant){
-        this.target = target;
-        this.composant = composant;
-    }
-}
-customElements.define('movie-element', MovieElement);
 customElements.define('movielist-element', MovielistElement);
 
 
